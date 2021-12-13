da90a7b37c6d9e2c1034964ce5d7e039eb78c3dd
SerebryakovD <serebrakov_d@ru.itstep.org>
2021-12-13 21:15:12 +0300

    004

--- main.cpp
+++ main.cpp
@@ -7,9 +7,31 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "");
 
-	issue1();
+	//issue1();
 	// раскоментировать строку ниже, чтобы нарисовать ромб.
 	//issue2();
+
+	int n, m;
+	char s;
+	cout << "¬ведите сторону а: ";
+	cin >> n;
+	cout << "¬ведите сторону b: ";
+	cin >> m;
+	cout << "¬ведите символ: ";
+	cin >> s;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (i == 0 || j == 0 || i == (n-1) || j == (m - 1)) {
+				cout << " " << s << " ";
+			}
+			else {
+				cout << "   ";
+			}
+		}
+
+		cout << endl;
+	}
 
 	return 0;
 }
